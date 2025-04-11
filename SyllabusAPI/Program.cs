d8c3c1a626ede994789843b2e0d085104977dd68
eglitafa88 <eglitafa88@gmail.com>
2025-04-12 00:07:25 +0200

    feature: add Swagger support for API documentation

--- SyllabusAPI/Program.cs
+++ SyllabusAPI/Program.cs
@@ -1,13 +1,23 @@
 using Syllabus.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Syllabus API",
+        Version = "v1",
+        Description = "API documentation for the Syllabus project"
+    });
+});
 
 var app = builder.Build();
 
@@ -16,6 +26,13 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Syllabus API v1");
+    c.RoutePrefix = string.Empty; //serve swagger at root
+});
 
 app.UseHttpsRedirection();
 
